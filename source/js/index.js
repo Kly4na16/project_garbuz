3e661522e154a6ab80976ae3d0c2e384c3438656
Kly4na16 <97173393+Kly4na16@users.noreply.github.com>
2023-02-23 14:38:49 +0200

    Add files via upload

---
+++ source/js/index.js
@@ -0,0 +1,18 @@
+const button = document.getElementsByTagName('button');
+
+const consBtn = document.getElementById('application');
+const form = document.getElementById('cons-form');
+
+consBtn.addEventListener('click',() => {
+   form.style.display = 'flex';
+})
+document.getElementById('close-btn').addEventListener('click',(e) => {
+   e.preventDefault();
+   form.style.display = 'none';
+})
+
+
+
+$(function () {
+   $("#beforeafter_foto").twentytwenty();
+});
