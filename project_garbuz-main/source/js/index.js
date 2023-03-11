7fc3e563f0124a0ba008b44e6000ab23801a9204
Kly4na16 <97173393+Kly4na16@users.noreply.github.com>
2023-03-11 20:39:52 +0200

    Add files via upload

---
+++ project_garbuz-main/source/js/index.js
@@ -0,0 +1,54 @@
+const button = document.getElementsByTagName('button');
+
+const consBtn = document.getElementById('application');
+const form = document.getElementById('cons-form');
+
+consBtn.addEventListener('click',() => {
+   form.style.display = 'flex';
+})
+document.getElementById('close-btn').addEventListener('click',(e) => {
+   e.preventDefault();
+   form.style.display = 'none';
+})
+
+//comment
+//comment
+const burger = document.getElementById('burger')
+const burgerMenu = document.getElementById('burger-menu')
+
+burger.addEventListener('click', () =>{
+   burgerMenu.style.display = (burgerMenu.style.display == 'none') ? 'block' : 'none'
+
+})
+if(window.screen.width>877){
+   burgerMenu.style.display = 'none';
+
+}
+const buyButton = document.getElementById('buy-button');
+buyButton.addEventListener('click', () => {
+   form.style.display = 'flex'
+})
+
+$(function () {
+   $("#beforeafter_foto").twentytwenty();
+});
+
+
+
+
+const slides = document.querySelectorAll(".slide");
+const intervalTime = 2000;
+let slideInterval;
+
+const nextSlide = () => {
+  const current = document.querySelector(".active");
+  current.nextElementSibling.classList.remove("active");
+  if (current.nextElementSibling) {
+    current.nextElementSibling.classList.add("active");
+  } else {
+    sliders[0].classList.add("active");
+  }
+};
+
+slideInterval = setInterval(nextSlide, intervalTime);
+
